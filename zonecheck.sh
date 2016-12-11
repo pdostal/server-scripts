b333ba8590f84301526092cfb458b70f7a57c484
pdostal <git@pdostal.cz>
2016-12-11 09:11:52 +0200

    Add some new and remove old as well as machine specific scripts

--- zonecheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-check-zone-expiration `cat all.rollrec | grep "^roll" | sed "s/\"//g" | sed "s/^roll.//g"` | grep "will expire"
-
--- zonerenew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for j in `cat all.rollrec | grep "^roll" | sed "s/\"//g" | sed "s/^roll.//g"`; do
-	touch db.$j
-	zonesigner -zone $j db.$j 1> /dev/null
-done
-service bind9 reload 1> /dev/null
-
